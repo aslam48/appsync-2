bc103480bb159f11d4ed9005af4def361755eda2
aslam48 <youngfizzy001@gmail.com>
2023-04-22 22:11:21 +0100

    working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "appsync2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+
 import '../styles/globals.css'
+import '../configureAmplify'
+import Navbar from './components/navbar'
+
+// import CreatePost from './create-post'
+import { useEffect, useState } from "react";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const [showChild, setShowChild] = useState(false);
 
+//   return(
+//     <div>
+//       <Navbar/>
+
+//       <div className='py-8 px-16 bg-slate-100'>
+//         {/* <Profile/> */}
+//       <Component {...pageProps} />
+//       </div>
+//     </div>
+
+//   ) 
+// }
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === "undefined") {
+    return <>
+      
+    </>;
+  } else {
+    return <>
+      <Navbar/>
+      <div className='py-8 px-16 bg-slate-100'>
+     <Component {...pageProps} />
+     </div>
+    </> 
+  }
+}
 export default MyApp
